30dfc504e74087f083a37d99788f99ed9bfe7251
Chirag Sankhla <116088829+chiragsankhla55@users.noreply.github.com>
2023-12-08 14:21:58 +0530

    JS Day 1
    js variable, js data type, js type conversion

---
+++ js day 1/test.js
@@ -0,0 +1,25 @@
+//js variables
+
+// const accountId = 12345
+// let accountName = "Chirag"
+// var accountEmail = "chirag@gmail.com"
+// accountCity = "jaipur"
+// console.table([accountId, accountName, accountEmail, accountCity])
+// console.log(accountName);
+
+
+//js data types
+// console.log(typeof accountEmail);
+// console.log(typeof accountId);
+
+
+//data conversion
+let score="chirag"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn="chirag"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
