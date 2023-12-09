a97f88ac3cd46623b44eaa2d71736dbe8f766a6a
Chirag Sankhla <116088829+chiragsankhla55@users.noreply.github.com>
2023-12-09 15:00:55 +0530

    js day 2
    js operators, comparision of data types, data type summary, stack and heap memory

---
+++ js day 2/day2.js
@@ -0,0 +1,77 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log("chirag"==02);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined ==0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// console.log("2"===2);
+
+// let name = "chirag"
+// const score = 100
+// const scorevalue = 100.3
+
+// console.table( [typeof name, typeof score, typeof scorevalue])
+
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail
+
+// console.table( [typeof isLoggedIn, typeof outSideTemp, typeof userEmail])
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.table([typeof id, typeof anotherId])
+// console.table([ id,  anotherId])
+// console.log(id==anotherId)
+// console.log(id===anotherId)
+
+// const bigInt = 121464665n
+// const Int = 121464665
+// console.table([typeof bigInt, typeof Int])
+
+// Array
+// const heros=["shaktimaan", "naagraj", "doga"];
+
+// object
+// let myObj={
+//     name:"hitesh",
+//     age:22,
+// }
+
+//function
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+
+// stack 
+// let userOne="chirag"
+// let userTwo=userOne
+// userTwo="Chiku"
+// console.log(userOne);
+// console.log(userTwo);
+
+
+// heap
+// let userOne={
+//     email:"chirag@google.com",
+//     upi:"chirag@ybl"
+// }
+// console.log(userOne.email)
+
+// let userTwo=userOne
+// userTwo.email ="ckiku@google.com"
+// console.log(userOne.email)
+// console.log(userTwo.email)
