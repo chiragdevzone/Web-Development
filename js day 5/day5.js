049a20180c7fd4c4dbdefaa7664eb9a03d64dd91
Chirag Sankhla <116088829+chiragsankhla55@users.noreply.github.com>
2023-12-12 20:27:15 +0530

    js day 5
    js arrays

---
+++ js day 5/day5.js
@@ -0,0 +1,43 @@
+// const myArr=[1,2,3,4,5];
+// const myHeros=["shaktimaan", "naagraj"];
+// const myArr2=new Array(1,2,"chirag","chiku");
+
+// console.log(myArr[2]);
+// console.log(myHeros[1]);
+// console.log(myArr2[1] +" "+myArr2[2]);
+// console.log(typeof myArr2[1] +" "+typeof myArr2[2]);
+// myArr.push(10);
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(15));
+// console.log(typeof myArr.join());
+
+// console.log(myArr.slice(1,3));
+// console.log(myArr);
+
+// console.log(myArr.splice(1,3));
+// console.log(myArr);
+
+// let marvel_heros=["thor","ironman","spiderman"];
+// let dc_heros=["superman","flash","batman"];
+// let allheros=marvel_heros.concat(dc_heros);
+// console.log(allheros);
+// let allheros=[...marvel_heros, ...dc_heros]
+// console.log(allheros);
+
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const real_another_array=another_array.flat(Infinity);
+// console.log(real_another_array);
+
+// console.log(Array.isArray("hitesh"));
+// console.log(Array.from("hitesh"));
+// console.log(Array.from({name:"hitesh"})); //interesting
+
+// let score1=100;
+// let score2=200;
+// let score3=300;
+// console.log(Array.of(score1,score2,score3));
+
